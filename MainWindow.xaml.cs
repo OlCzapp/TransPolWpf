ad47499cf14f5415df3e68b9def5c9e27770b8e7
Oliwia <oliwia.czap13@gmail.com>
2026-02-05 13:27:37 +0100

    Dodanie trwałego zapisu i wyświetlania listy pojazdów
    Dodano obsługę zapisu pojazdów do pliku "lista_pojazdow.txt" oraz automatyczne wczytywanie i wyświetlanie ich w aplikacji. Zmieniono interfejs: domyślnie zaznaczony jest typ "Osobowy", a lista pojazdów wyświetlana jest w ScrollViewerze z paskami przewijania. Po dodaniu pojazdu lista jest odświeżana.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System;
+using System.IO;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,11 +21,52 @@ namespace TransPolWpf
         public MainWindow()
         {
             InitializeComponent();
+            WczytajPojazdy();
+        }
+
+        void WczytajPojazdy()
+        {
+            string sciezka = "lista_pojazdow.txt";
+
+            if (File.Exists(sciezka))
+            {
+                wyswietlany_text.Text = File.ReadAllText(sciezka, Encoding.UTF8); //wyswietlany_text - textblock po prawej stronie, w którym wyświetlamy dane z pliku
+            }
+            else
+            {
+                wyswietlany_text.Text = "Brak danych.";
+            }
         }
 
         private void dodaj_Click(object sender, RoutedEventArgs e)
         {
+            string marka_cs = marka.Text;
+            string model_cs = model.Text;
+            string rok_cs = rok_produkcji.Text;
 
+            string rodzaj_cs = "";
+            if (a.IsChecked == true)
+            {
+                rodzaj_cs = "Osobowy";
+            }
+            else if (b.IsChecked == true)
+            {
+                rodzaj_cs = "Ciężarowy";
+            }
+            else
+            {
+                rodzaj_cs = "Motocykl";
+            }
+
+            string linia = $"Dane: \nMarka: {marka_cs}\nModel: {model_cs}\nRok produkcji: {rok_cs}\nRodzaj: {rodzaj_cs}\n...";
+
+            File.AppendAllText("lista_pojazdow.txt", linia + Environment.NewLine);
+
+            WczytajPojazdy(); //odświeżenie TextBlocka
+
+            //wyswietlany_text.Text = $"Dane: \nMarka: {marka_cs}\nModel: {model_cs}\nRok produkcji: {rok_cs}\nRodzaj: {rodzaj_cs}\n...";
+
+            //MessageBox.Show($"Dodano pojazd:\nMarka: {marka_cs}\nModel: {model_cs}\nRok produkcji: {rok_cs}\nRodzaj: {rodzaj_cs}", "Informacja", MessageBoxButton.OK, MessageBoxImage.Information);
         }
     }
 }
